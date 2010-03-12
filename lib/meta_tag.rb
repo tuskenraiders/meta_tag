aaa7076db6eaf9cc5b5eedd34d535f19d8cc6c4c
Andreas Riemer <andreas.riemer@gmail.com>
2010-03-12 16:13:22 +0000

    CHANGED Ueberall im trunk require 'activesupport' und require 'activerecord' durch require 'active_support' und require 'active_record' ersetzt
    refs #2531353
    reviewed ffrank
    
    git-svn-id: https://svn.pkw.de/pkw.de/trunk/gems/meta_tag@9343 a0138c44-f643-4d49-8908-3bd568d7ed4a

--- lib/meta_tag.rb
+++ lib/meta_tag.rb
@@ -1,8 +1,8 @@
-require "activesupport"
+require "active_support"
 require "meta_tag/tag"
 require "meta_tag/page_definition"
 require "meta_tag/title_tag"
 
 module MetaTag
-  VERSION = '0.2.0'
+  VERSION = '0.2.1'
 end
